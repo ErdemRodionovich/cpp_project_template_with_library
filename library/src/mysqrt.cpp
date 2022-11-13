5f5269626d4cfbeceee8276a4cbfbe9a80ec9b4d
Erdem Badluev <erdembad@rambler.ru>
2022-11-13 19:46:52 +0300

    step 7 cmake tutorial

--- library/src/mysqrt.cpp
+++ library/src/mysqrt.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+#include <cmath>
+#endif
+
 // a hack square root calculation using simple operations
 double mysqrt(double x)
 {
@@ -10,6 +14,11 @@ double mysqrt(double x)
   double result = x;
 
   // do ten iterations
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  result = std::exp(std::log(x) * 0.5);
+  std::cout << "Computing sqrt of " << x << " to be " << result
+            << " using log and exp" << std::endl;
+#else
   for (int i = 0; i < 10; ++i) {
     if (result <= 0) {
       result = 0.1;
@@ -18,5 +27,6 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
+#endif
   return result;
 }
