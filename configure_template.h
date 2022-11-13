71baeb3e07039ca4d5ee6e97e974436025c1774f
Erdem Badluev <erdembad@rambler.ru>
2022-11-13 14:48:17 +0300

    Start develop credentials tool

---
+++ configure_template.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace ertb{
+constexpr int VERSION_MAJOR = ${${PROJECT_NAME}_VERSION_MAJOR};
+constexpr int VERSION_MINOR = ${${PROJECT_NAME}_VERSION_MINOR};
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "configure.h"
+
+int main(int argc, char ** argv){
+    std::cout<<"Hello world! MajorVersion="<<ertb::VERSION_MAJOR<<" Minor="<<ertb::VERSION_MINOR<<std::endl;
+    return 0;
+}
