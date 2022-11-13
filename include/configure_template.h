e11394b1f3bcbf4e6b73f5b6ea2e51d883cb1dd5
Erdem Badluev <erdembad@rambler.ru>
2022-11-13 15:54:31 +0300

    step 2 cmake tutorial ready

--- include/configure_template.h
+++ include/configure_template.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#cmakedefine USE_MYMATH;
+
 namespace ertb{
 constexpr int VERSION_MAJOR = ${${PROJECT_NAME}_VERSION_MAJOR};
 constexpr int VERSION_MINOR = ${${PROJECT_NAME}_VERSION_MINOR};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include "configure.h"
 
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#else
+#include <cmath>
+#endif
+
 int main(int argc, char ** argv){
     std::cout<<"Hello world! MajorVersion="<<ertb::VERSION_MAJOR<<" Minor="<<ertb::VERSION_MINOR<<std::endl;
+    std::cout<<"Sqrt(4)="<<
+#ifdef USE_MYMATH
+    mysqrt(4)
+#else
+    std::sqrt(4)
+#endif
+    <<std::endl;
     return 0;
 }
