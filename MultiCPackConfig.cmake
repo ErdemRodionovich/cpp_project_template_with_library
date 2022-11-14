4a5c6ede1aca7a50ff6482faa6ea044835a123ef
Erdem Badluev <erdembad@rambler.ru>
2022-11-14 09:10:44 +0300

    change project name

--- MultiCPackConfig.cmake
+++ MultiCPackConfig.cmake
@@ -2,6 +2,6 @@ include("release/CPackConfig.cmake")
 
 #use the CPACK_INSTALL_CMAKE_PROJECTS variable to specify which projects to install
 set(CPACK_INSTALL_CMAKE_PROJECTS
-    "debug;credentialsTool;ALL;/"
-    "release;credentialsTool;ALL;/"
+    "debug;projectTemplate;ALL;/"
+    "release;projectTemplate;ALL;/"
 )
