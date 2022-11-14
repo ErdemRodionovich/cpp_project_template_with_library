485420528b483515559adcdce44a52ee82b660d5
Erdem Badluev <erdembad@rambler.ru>
2022-11-14 08:50:37 +0300

    Step 12 cmake tutorial

---
+++ MultiCPackConfig.cmake
@@ -0,0 +1,7 @@
+include("release/CPackConfig.cmake")
+
+#use the CPACK_INSTALL_CMAKE_PROJECTS variable to specify which projects to install
+set(CPACK_INSTALL_CMAKE_PROJECTS
+    "debug;credentialsTool;ALL;/"
+    "release;credentialsTool;ALL;/"
+)
